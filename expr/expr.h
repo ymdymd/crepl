d84aa271435db449b1cb4e8a4b359a1a8543c7a8
Isao Yamada <yamada@deep-i.co.jp>
2017-06-26 17:14:31 +0900

    split code.

---
+++ expr/expr.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <memory>
+#include <string>
+
+namespace expr {
+
+//-----------------------------------------------------------------------------
+// AST (Abstract Syntax Tree)
+class ExprAST {
+public:
+	virtual ~ExprAST() {}
+	virtual int eval(int(*fp)(std::string&) = nullptr) { return 0; }
+};
+
+//-----------------------------------------------------------------------------
+// evalute expr_str
+std::unique_ptr<ExprAST> parser(const std::string expr_str);
+
+//-----------------------------------------------------------------------------
+// evalute expr_str
+int eval(const std::string expr_str, int(*fp)(std::string&) = nullptr);
+
+}	//namespace expr
